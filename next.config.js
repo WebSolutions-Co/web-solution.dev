86268c9a65cfbddb4e820a0452f7752e9d79b4eb
encryptedDegen <encrypted@koyo.finance>
2024-01-03 20:51:49 +0100

    Revert "bump up the static generation timeout"
    This reverts commit 1ce6f431445adb094631756a71e937a6491ead59.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	staticPageGenerationTimeout: 120,
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
