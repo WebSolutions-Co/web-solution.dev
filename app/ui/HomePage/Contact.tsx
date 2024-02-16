4ef7e4796425b0375e0cd60f052b7f84b2946750
encryptedDegen <encrypted@kodex.io>
2024-02-16 18:51:35 +0100

    build fixes

--- app/ui/HomePage/Contact.tsx
+++ app/ui/HomePage/Contact.tsx
@@ -2,15 +2,13 @@
 
 import React from 'react';
 import SubHeading from '../sub-heading';
-import { PopupButton, PopupWidget } from 'react-calendly';
+import { PopupButton } from 'react-calendly';
 
 const Contact = () => {
-	const rootEl = document.body;
-
 	return (
 		<div className='w-full max-w-5xl flex flex-col gap-8 xl:px-0'>
 			<SubHeading>
-				Let's hop on a <span className='text-green-500'>Call</span>
+				Let&apos;s hop on a <span className='text-green-500'>Call</span>
 			</SubHeading>
 			<div>
 				<p>Amazing project idea?</p>
@@ -19,14 +17,14 @@ const Contact = () => {
 			<div>
 				<p>Growing your team?</p>
 			</div>
-			{rootEl && (
+			{typeof window !== 'undefined' && (
 				<PopupButton
 					url='https://calendly.com/jan-zunec/30-min-web-solutions'
 					/*
 					 * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
 					 * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
 					 */
-					rootElement={rootEl}
+					rootElement={document.body}
 					text='Schedule a 30min call with us!'
 					className='bg-green-500 max-w-72 h-10 font-semibold rounded-md'
 				/>
