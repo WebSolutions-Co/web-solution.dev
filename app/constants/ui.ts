d43b4dd469922b193986bbe45b53a021ce0c667c
encryptedDegen <encrypted@kodex.io>
2024-03-01 21:27:30 +0100

    change testimonials

--- app/constants/ui.ts
+++ app/constants/ui.ts
@@ -8,25 +8,19 @@ export const testimonials = [
 	{
 		id: 0,
 		quote: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.',
-		name: 'James Kokonas - co-founder',
+		name: 'James Kokonas - Co-founder',
 		title: 'Kodex Solutions',
 	},
 	{
 		id: 1,
 		quote: 'It was a pleasure working with them. The team has managed the whole project from start to the finish line, to deliver a quality product very quickly. They have respected our wishes and ideas, and they even improved them. If we ever need another digital product, we are definitely going with them. Highly recommend!',
-		name: 'Branko Žunec',
+		name: 'Branko Žunec - Founder and CEO',
 		title: 'BMC International',
 	},
 	{
 		id: 2,
 		quote: 'They are some of the most diligent, hard working devs I encountered in the web3 ecosystem. Always open to tackling new challenges, and highly responsive and adaptable. I was seriously impressed with how they managed to learn the nuances of our stack, to integrate with various other protocols as well as our complex backend. it was always a pleasure to work with them.',
-		name: 'MevBadnit - co-founder',
+		name: 'MevBadnit - Co-founder',
 		title: 'Kodex Solutions',
-	},
-	{
-		id: 3,
-		quote: 'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.',
-		name: 'Jane Austen',
-		title: 'Pride and Prejudice',
 	},
 ];
